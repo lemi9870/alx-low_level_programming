5f7886a67f68026c8514066ff8b9740ff2c380cb
lemi9870 <lemibelay9870@gmail.com>
2023-02-16 05:11:13 -0800

    jffjfjf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Sucess)
  */
-int main(void){
+int main(void)
+{
 
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

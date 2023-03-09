8f0b207aff44f187df65ce3e911e7cd5e2f06db6
lemi9870 <lemibelay9870@gmail.com>
2023-03-09 03:54:51 -0800

    dgdgdg

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string.
- * @s: pointer to char
- * Return: No.
+ * factorial - factorial n
+ * @n: integer arams
+ * Return: recursion
  */
 
-void _puts_recursion(char *s)
+int factorial(int n)
 {
-	if (*s != '\0')
+	if (n < 0)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		return (-1);
 	}
-	else
+	if (n == 1)
 	{
-	_putchar('\n');
+		return (1);
 	}
+	return (n * factorial(n - 1));
 }
-
 
